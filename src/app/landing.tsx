5623c06f58ce4ba0af13eacd4d28958090e31bc7
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-01-27 18:32:00 -0800

    style: landing

--- src/app/landing.tsx
+++ src/app/landing.tsx
@@ -1,22 +1,40 @@
+import Image from "next/image";
 import "./landing.css";
 
 export default function Landing() {
   return (
     <div>
-      <h1 className="h1">Welcome to Challenge Mates!</h1>
+      <div className="grid h-[calc(100vh-4rem)] place-content-center bg-gradient-to-b from-cyan-50 to-white">
+        <h1 className="text-balance bg-gradient-to-tr from-indigo-700 to-cyan-500 bg-clip-text p-6 text-center text-8xl font-bold text-transparent">
+          Welcome to Challenge Mates!
+        </h1>
+        <p className="mx-auto mt-6 max-w-2xl px-6 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          pharetra, est commodo molestie molestie, est eros fermentum leo, sed
+          blandit felis ligula ut lacus. Integer ultrices mollis quam malesuada
+          feugiat. Vestibulum feugiat scelerisque semper. Maecenas eget placerat
+          lectus. Pellentesque tristique ornare turpis, vel congue urna mattis
+          in.
+        </p>
+        <div className="mt-6 w-full">
+          <Image
+            className="img"
+            src="/logo.png"
+            alt=""
+            width="200"
+            height="200"
+          />
+        </div>
+      </div>
 
-<img className="img" src="logo.png" width="200" height="200"></img>
+      <h2 className="h2">Empowering Lives, Ending Homelessness Together</h2>
 
-<p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra, est commodo molestie molestie, est eros fermentum leo, sed blandit felis ligula ut lacus. Integer ultrices mollis quam malesuada feugiat. Vestibulum feugiat scelerisque semper. Maecenas eget placerat lectus. Pellentesque tristique ornare turpis, vel congue urna mattis in. Aliquam ac nisi arcu. In hac habitasse platea dictumst. Proin hendrerit vestibulum diam nec malesuada. Nullam accumsan imperdiet blandit. Aliquam tempus, purus non accumsan dapibus, ex velit accumsan diam, eget tincidunt tortor mauris non tortor. Duis vehicula tortor in massa pellentesque, a egestas orci suscipit. Morbi non lacus porta massa hendrerit ultricies quis ac tellus.
-</p>
-
-<h2 className="h2">Empowering Lives, Ending Homelessness Together</h2>
-
-<button className="button1" type="button">Sign In</button>
-<button className="button2" type="button">Donate</button>
-   
-<footer className="footer"> © Challange Mates </footer>
-
+      <button className="button1" type="button">
+        Sign In
+      </button>
+      <button className="button2" type="button">
+        Donate
+      </button>
     </div>
   );
 }
